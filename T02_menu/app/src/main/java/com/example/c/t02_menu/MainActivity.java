ddaa84718baf1885a273a26545c7c9068437aca9
tbs80 <eltcple@naver.com>
2016-05-07 18:19:52 +0900

    1234

--- T02_menu/app/src/main/java/com/example/c/t02_menu/MainActivity.java
+++ T02_menu/app/src/main/java/com/example/c/t02_menu/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.c.t02_menu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,10 +15,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView)findViewById(R.id.textViewTest);
+        registerForContextMenu(tv);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menuChoco) {
+            Toast.makeText(this, "초코우유 메뉴가 선택되었다.", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "딸기우유 메뉴가 선택되었다.", Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+        menu.add(0, 101, 0, "버거킹");
+        menu.add(0, 102, 0, "맥도날드");
+        menu.add(0, 103, 0, "롯데리아");
+        menu.add(0, 104, 0, "파파이스");
+        menu.add(0, 105, 0, "KFC");
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_LONG).show();
+        return super.onContextItemSelected(item);
     }
 }

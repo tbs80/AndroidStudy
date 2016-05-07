1dcedd0c1723ff76ba4e5a83b56bb2ad43dc8784
tbs80 <eltcple@naver.com>
2016-05-07 11:21:50 +0900

    가나다라마바사

--- HelloWorld/app/src/main/java/com/example/c/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/c/helloworld/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("a", "시발");
+        Log.d("a", "개씨발");
     }
 }
